e4e41c30bb08a32802d10540e3225a0e352e6b0c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-13 20:48:09 +0600

    Create GET API To Get Application Data

--- index.js
+++ index.js
@@ -152,6 +152,13 @@ async function run() {
             res.send(result);
         });
 
+        // secure api|| moderator only|| jwt validation
+        app.get('/applications', async (req, res) => {
+            const cursor = applicationCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
