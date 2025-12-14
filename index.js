a2b301993c75514dae581ff655577eae39710759
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-14 12:51:34 +0600

    Create Middleware To Verify Moderator Authorization

--- index.js
+++ index.js
@@ -81,6 +81,18 @@ async function run() {
             next();
         }
 
+        const verifyModerator = async (req, res, next) => {
+            const email = req.decoded_email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+
+            if (!user || user.role !== 'moderator') {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
+            next();
+        }
+
         // create user | login, register, social login page
         app.post('/users', async (req, res) => {
             const user = req.body;
