b4a630701ff4e4295773f9b195c52baadbd7fd6b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-19 11:06:48 +0600

    Secure GET API To Get Individual Scholarship Data By JWT Token Vrification

--- index.js
+++ index.js
@@ -184,9 +184,8 @@ async function run() {
             res.send(result);
         });
 
-        // get individual scholarship || secure api
-        app.get('/scholarship/:id', async (req, res) => {
-
+        // get individual scholarship || secure api | JWT Verified | used in Scholarship Details Page
+        app.get('/scholarship/:id', verifyJWTToken, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await scholarshipCollection.findOne(query);
