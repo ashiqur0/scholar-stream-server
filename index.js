cdf3401e33ac4aa411455eac58d3105717eec4fd
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-19 12:12:07 +0600

    Secure GET API To Get Summary Of Application Status  By JWT Token Vrification And Restrict It By Accessible Only For Admin

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         // create database
         const db = client.db('scholar-stream');
@@ -366,8 +366,8 @@ async function run() {
             res.send(result);
         });
 
-        // mongodb aggregate pipeline
-        app.get('/applications/application-status/stats', async (req, res) => {
+        // mongodb aggregate pipeline | secure api | JWT Verified | Moderator Verified | Used In Analytics Page
+        app.get('/applications/application-status/stats', verifyJWTToken, verifyAdmin, async (req, res) => {
             const pipeline = [
                 {
                     $group: {
@@ -432,8 +432,8 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
