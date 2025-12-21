deb6f1b7d99d67cd9a99e97770ef4a75c37e3abb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-21 10:31:44 +0600

    Update POST API, When Will Apply For Scholarship Store There Profile Image Too

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // create database
         const db = client.db('scholar-stream');
@@ -317,6 +317,7 @@ async function run() {
                     scholarshipName: applicationInfo.scholarshipName,
                     userId: applicationInfo.userId,
                     userName: applicationInfo.userName,
+                    userImage: applicationInfo.userImage,
                     universityName: applicationInfo.universityName,
                     scholarshipCategory: applicationInfo.scholarshipCategory,
                     degree: applicationInfo.degree,
@@ -353,6 +354,7 @@ async function run() {
                     scholarshipName: session.metadata.scholarshipName,
                     userName: session.metadata.userName,
                     userId: session.metadata.userId,
+                    userImage: session.metadata.userImage,
                     universityName: session.metadata.universityName,
                     scholarshipCategory: session.metadata.scholarshipCategory,
                     degree: session.metadata.degree,
@@ -591,8 +593,8 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
